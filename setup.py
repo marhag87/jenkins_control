4ad990dcacadddfbe9d19da9289902f927426cff
Martin Hagstrom <marhag87@gmail.com>
2017-06-14 12:28:27 +0200

    Release v0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='marhag87',
     author_email='marhag87@gmail.com',
     url='https://github.com/marhag87/jenkins_control',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     license='WTFPL',
     description='Simple controls for jenkins',
